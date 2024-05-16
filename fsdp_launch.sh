422bd30d5c777f46b5fe6ac0117542f316283ba4
Maxime De Bruyn <maximedebruyn@gmail.com>
2024-05-16 19:24:58 +0000

    fsdp

---
+++ fsdp_launch.sh
@@ -0,0 +1,42 @@
+export HF_TOKEN=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
+accelerate launch --config_file "fsdp_config_qlora.yaml"  train.py \
+--seed 100 \
+--model_name_or_path "meta-llama/Meta-Llama-3-8B-Instruct" \
+--train_filename "datasets/no_system_prompt/train.jsonl" \
+--chat_template_format "chatml" \
+--add_special_tokens False \
+--append_concat_token False \
+--splits "train,test" \
+--max_seq_len 128 \
+--num_train_epochs 0.05 \
+--logging_steps 5 \
+--log_level "info" \
+--logging_strategy "steps" \
+--evaluation_strategy "epoch" \
+--save_strategy "epoch" \
+--push_to_hub \
+--hub_private_repo True \
+--hub_strategy "every_save" \
+--bf16 True \
+--packing True \
+--learning_rate 1e-4 \
+--lr_scheduler_type "cosine" \
+--weight_decay 1e-4 \
+--warmup_ratio 0.0 \
+--max_grad_norm 1.0 \
+--output_dir "llama-sft-qlora-fsdp" \
+--per_device_train_batch_size 2 \
+--per_device_eval_batch_size 2 \
+--gradient_accumulation_steps 2 \
+--gradient_checkpointing True \
+--use_reentrant True \
+--use_flash_attn True \
+--use_peft_lora True \
+--lora_r 8 \
+--lora_alpha 16 \
+--lora_dropout 0.1 \
+--lora_target_modules "all-linear" \
+--use_4bit_quantization True \
+--use_nested_quant True \
+--bnb_4bit_compute_dtype "bfloat16" \
+--bnb_4bit_quant_storage_dtype "bfloat16"
--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 pip install transformers datasets peft accelerate trl bitsandbytes
-pip install flash-attn --no-build-isolation
+pip install flash-attn
+git config --global user.email "maximedebruyn@gmail.com"
+git config --global user.name "Maxime De Bruyn"
