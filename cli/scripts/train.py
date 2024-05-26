807eacc2404db6fe0144fc4d5231ac215ffbaf89
Maxime De Bruyn <maximedebruyn@gmail.com>
2024-05-26 12:02:14 +0000

    ok

--- cli/scripts/train.py
+++ cli/scripts/train.py
@@ -2,13 +2,24 @@
 
 import os
 import sys
-from dataclasses import dataclass, field
 from typing import Optional
+from dataclasses import dataclass, field
 
 from transformers import HfArgumentParser, TrainingArguments, set_seed
 from trl import SFTTrainer
 from utils import create_and_prepare_model, create_datasets
-from datasets import load_dataset
+from enum import Enum
+
+import torch
+from datasets import DatasetDict, load_dataset, load_from_disk
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+)
+
+from peft import LoraConfig
+
 
 
 # Define and parse arguments.
@@ -99,32 +110,66 @@ class DataTrainingArguments:
     )
 
 
-def apply_chat_template(example, tokenizer):
-    messages = example["messages"]
-    example["text"] = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=False, # only used for generation purposes
-    )
-    return example
-
-
-def create_dataset_new(data_args, tokenizer):
-    # Load dataset
+def get_datasets(data_args):
     data_files = {"train": data_args.train_filename}
     if data_args.valid_filename is not None:
         data_files["validation"] = data_Args.valid_filename
     raw_dataset = load_dataset("json", data_files=data_files)
+    return raw_dataset["train"], raw_dataset.get("validation", None)
 
-    # Format
-    raw_dataset = raw_dataset.map(
-        apply_chat_template,
-        fn_kwargs={"tokenizer": tokenizer},
-        remove_columns=list(raw_dataset["train"].features),
-        desc="Applying chat template",
+
+def create_and_prepare_model(args, data_args, training_args):
+    bnb_config = None
+    quant_storage_dtype = None
+
+    if args.use_4bit_quantization:
+        compute_dtype = getattr(torch, args.bnb_4bit_compute_dtype)
+        quant_storage_dtype = getattr(torch, args.bnb_4bit_quant_storage_dtype)
+
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=args.use_4bit_quantization,
+            bnb_4bit_quant_type=args.bnb_4bit_quant_type,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=args.use_nested_quant,
+            bnb_4bit_quant_storage=quant_storage_dtype,
+        )
+
+        if compute_dtype == torch.float16 and args.use_4bit_quantization:
+            major, _ = torch.cuda.get_device_capability()
+            if major >= 8:
+                print("=" * 80)
+                print("Your GPU supports bfloat16, you can accelerate training with the argument --bf16")
+                print("=" * 80)
+        elif args.use_8bit_quantization:
+            bnb_config = BitsAndBytesConfig(load_in_8bit=args.use_8bit_quantization)
+
+    torch_dtype = (
+        quant_storage_dtype if quant_storage_dtype and quant_storage_dtype.is_floating_point else torch.float32
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_name_or_path,
+        quantization_config=bnb_config,
+        trust_remote_code=True,
+        attn_implementation="flash_attention_2" if args.use_flash_attn else "eager",
+        torch_dtype=None if args.use_flash_attn else torch_dtype,
     )
 
-    return raw_dataset["train"], raw_dataset.get("validation", None)
+    peft_config = LoraConfig(
+        base_model_name_or_path=args.model_name_or_path,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
+        r=args.lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=args.lora_target_modules.split(",")
+        if args.lora_target_modules != "all-linear"
+        else args.lora_target_modules,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, peft_config, tokenizer
 
 
 def main(model_args, data_args, training_args):
@@ -133,11 +178,6 @@ def main(model_args, data_args, training_args):
 
     # model
     model, peft_config, tokenizer = create_and_prepare_model(model_args, data_args, training_args)
-    vocab_size = getattr(getattr(model, "config", None), "vocab_size", None)
-    model_id = getattr(peft_config, "base_model_name_or_path", None)
-    print(vocab_size)
-    print(model_id)
-    print(model.config.__class__.from_pretrained(model_id).vocab_size)
 
     # gradient ckpt
     model.config.use_cache = not training_args.gradient_checkpointing
@@ -146,9 +186,7 @@ def main(model_args, data_args, training_args):
         training_args.gradient_checkpointing_kwargs = {"use_reentrant": model_args.use_reentrant}
 
     # datasets
-    train_dataset, eval_dataset = create_dataset_new(data_args, tokenizer)
-    print(train_dataset)
-    print(eval_dataset)
+    train_dataset, eval_dataset = get_datasets(data_args)
 
     # trainer
     trainer = SFTTrainer(
@@ -156,14 +194,13 @@ def main(model_args, data_args, training_args):
         tokenizer=tokenizer,
         args=training_args,
         train_dataset=train_dataset,
-        #eval_dataset=eval_dataset,
+        eval_dataset=eval_dataset,
         peft_config=peft_config,
         packing=data_args.packing,
         dataset_kwargs={
             "append_concat_token": data_args.append_concat_token,
             "add_special_tokens": data_args.add_special_tokens,
         },
-        dataset_text_field="text",
         max_seq_length=data_args.max_seq_length,
     )
     trainer.accelerator.print(f"{trainer.model}")
