8bf11875535884b96c65865652238723842eb0c0
Maxime De Bruyn <maximedebruyn@gmail.com>
2024-06-18 15:35:48 +0000

    ok

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 export HF_TOKEN=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
-pip install transformers datasets peft accelerate==0.30.1 trl bitsandbytes
-pip install flash-attn
+pip install transformers==4.41.2 datasets==2.20.0 peft==0.11.1 accelerate==0.30.1 trl==0.9.4 bitsandbytes==0.43.1
+pip install flash-attn==2.5.9.post1
 git config --global user.email "maximedebruyn@gmail.com"
 git config --global user.name "Maxime De Bruyn"
 python tmp/create_datasets.py
