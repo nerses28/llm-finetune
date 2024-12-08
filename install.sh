939bfaa20f9c9887b8b22ad2a60d58ff856991cf
nerses28 <nerses28@gmail.com>
2024-12-08 16:39:02 +0100

    update install launch scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-export HF_TOKEN=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
-pip install wandb==0.17.2 transformers==4.41.2 datasets==2.20.0 peft==0.11.1 accelerate==0.30.1 trl==0.9.4 bitsandbytes==0.43.1
-pip install flash-attn==2.5.9.post1
+export HF_TOKEN= #ADD YOUR HF KEY
+pip3 install wandb==0.17.2 transformers==4.43.1 datasets==2.20.0 peft==0.11.1 accelerate==0.30.1 trl==0.9.4 bitsandbytes==0.43.1 fastapi==0.100.0 uvicorn==0.22.0
+
+#pip install flash-attn==2.5.9.post1
+wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.0.post1/flash_attn-2.7.0.post1+cu12torch2.5cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
+pip3 install --upgrade flash_attn-2.7.0.post1+cu12torch2.5cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
+
 git config --global user.email "maximedebruyn@gmail.com"
 git config --global user.name "Maxime De Bruyn"
-python tmp/create_datasets.py
-pip install -e .
+python3 tmp/create_datasets.py
+pip3 install -e .
--- launch.sh
+++ launch.sh
@@ -1,5 +1,19 @@
 # pip install flash-attn --no-build-isolation
-export HF_TOKEN=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
+export HF_TOKEN= #ADD YOUR HF KEY
 
-clipsai train --data_train /clips-ai-train/datasets/ultrachat/train.jsonl --data_valid /clips-ai-train/datasets/ultrachat/validation.jsonl
-clipsai align --data_train /clips-ai-train/datasets/ultrachat_dpo/train.jsonl --data_valid /clips-ai-train/datasets/ultrachat_dpo/test.jsonl --peft_path test
+#export NCCL_DEBUG=INFO
+#export TORCH_NCCL_BLOCKING_WAIT=1
+#export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+
+#export NCCL_DEBUG=INFO
+#export NCCL_P2P_LEVEL=NVL
+#export NCCL_IB_DISABLE=1  
+#export NCCL_SOCKET_IFNAME=eth0 
+
+#clipsai train --data_train datasets/ultrachat/train.jsonl --data_valid datasets/ultrachat/validation.jsonl
+
+#clipsai train --data_train datasets/ultrachat/validation.jsonl --data_valid datasets/ultrachat/validation.jsonl
+clipsai predict --data_test datasets/ultrachat/test.jsonl --peft_path test/ --batch_size 1 --output_filename predictions2.jsonl
+
+
+#clipsai align --data_train datasets/ultrachat_dpo/train.jsonl --data_valid datasets/ultrachat_dpo/test.jsonl --peft_path test --model_suffix experiment1
